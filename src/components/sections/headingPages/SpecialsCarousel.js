69cd733e706e25b2845317b1bf2c71c7441e983e
Etay Nir <mac@Mac-mini.homenet.telecomitalia.it>
2024-03-12 11:31:36 +0100

    updated SpecialCarousel.js

---
+++ src/components/sections/headingPages/SpecialsCarousel.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Carousel } from 'react-responsive-carousel';
+import GreekSalad from '../../../assets/food/greeg-salad.jpg';
+import Bruschetta from '../../../assets/food/bruschetta.jpeg';
+import LemonDessert from '../../../assets/food/lemon desssert.jpeg';
+import SpecialCard from '.CardInfo/SpecialCard';
+
+function CaruselPage() {
+    return (
+        <Carousel infiniteLoop={true} autoPlay={true} interval={5000} showStatus={false}>
+            <SpecialCard image={GreekSalad} name="Greek Salad" price="$12.99" description="Feta cheese, tomato, lettuce"/>
+            <SpecialCard image={Bruschetta} name="Bruschetta" price="$9.99" description="Bread, mango, green onions"/>
+            <SpecialCard image={LemonDessert} name="Lemon Dessert" price="$7.99" description="Lemon tart, vanilla icing"/>
+        </Carousel>
+    );
+}
+
+export default CaruselPage;
