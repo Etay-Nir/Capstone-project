957b1a2cac9270db4b45b967dc56ba74b27a39f3
Etay Nir <mac@Mac-mini.homenet.telecomitalia.it>
2024-03-13 10:18:25 +0100

    updated Routing.js

--- src/components/Routing.js
+++ src/components/Routing.js
@@ -1,9 +1,20 @@
 import { Routes, Route } from "react-router-dom";
+import Homepage from './pages/HomePage';
+import About from './pages/About';
+import Reservation from './pages/BookingPage';
+import Order from './pages/Order';
+import Login from './pages/Login';
+import Confirmation from './pages/Confirmation';
 
 function Routing() {
     return (
         <Routes>
-            
+            <Route path="/" element={<Homepage />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/reservation" element={<Reservation />} />
+            <Route path="/order" element={<Order />} />
+            <Route path="login" element={<Login />} />
+            <Route path="/confirmation" element={<Confirmation />} />
         </Routes>
     );
 }
