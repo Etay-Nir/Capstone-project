27dd53a90e3758782db0e54af7ae54054c494afb
Etay Nir <mac@Mac-mini.homenet.telecomitalia.it>
2024-03-13 10:04:55 +0100

    updated TestimonialCarousel.js

---
+++ src/components/sections/headingPages/TestimonialCarousel.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {Carousel} from 'react-responsive-carousel';
+import TestimonialCard from './CardInfo/TestimonialCard';
+
+function CarouselPage() {
+    return (
+        <Carousel>
+            <TestimonialCard name="Michal Nir" description="This is one of my favorite plave to have dinner."/>
+            <TestimonialCard name="Oran Keret" description="The food is always fresh, I love to order in."/>
+            <TestimonialCard name="Adam Lahav" description="Little Lemon is the plcace to go if you want great food."/>
+            <TestimonialCard name="Ido Shachar" description="Great food, I cannot have a meal without the Lemon dessert."/>
+            <TestimonialCard name="Liron Feldman" description="I love the athmosphere in the restaurant."/>
+            <TestimonialCard name="Yoed Smol" description="Best place to come with friends and have a great food."/>
+            <TestimonialCard name="Ron Winner" description="The shippment is fast and the food arrives fresh and hot."/>
+            <TestimonialCard name="Kobi Lin" description="Service is always top nuch and the food is tasty."/>
+        </Carousel>
+    );
+}
+
+export default CarouselPage;
